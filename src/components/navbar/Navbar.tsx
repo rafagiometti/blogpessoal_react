eaf84f5a56d17caa23835f59484a53aac044385a
rafaela_giometti <rafaela_giometti@hotmail.com>
2025-06-25 18:09:02 -0300

    :sparkles: Finalizando CRUD

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,38 @@
+import { useContext } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import { AuthContext } from "../../contexts/AuthContext"
+
+function Navbar() {
+
+    const navigate = useNavigate()
+
+    const { handleLogout } = useContext(AuthContext)
+
+    function logout(){
+        handleLogout()
+        alert("O usuário foi desconectado com sucesso!")
+        navigate("/")
+    }
+
+    return (
+        <>
+            <div className='w-full flex justify-center py-4
+            			   bg-indigo-900 text-white'>
+            
+                <div className="container flex justify-between text-lg mx-8">
+                    <Link to="/home" className="text-2xl font-bold">Blog Pessoal</Link>
+
+                    <div className='flex gap-4'>
+                        Postagens
+                        <Link to='/temas' className='hover:underline'>Temas</Link>
+                        <Link to='/cadastrartema' className='hover:underline'>Cadastrar tema</Link>
+                        Perfil
+                        <Link to="" onClick={logout} className="hover:underline">Sair</Link>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
